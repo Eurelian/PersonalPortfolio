f871412b12e676881317c17b91755b8510b7d63d
Ion Vasilescu <sumoctavian@gmail.com>
2021-11-22 08:25:01 +0100

    update

--- src/components/MyRecentWork.js
+++ src/components/MyRecentWork.js
@@ -10,139 +10,138 @@ import { projects } from "../data/projects.json";
 import { CardActionArea } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		marginTop: "150px",
-		marginBottom: "200px",
-	},
-	title: {
-		fontFamily: "Teko",
-		color: "#f5f5f5",
-		textShadow: "1px 1px 3px rgba(0, 0, 0, 0.6)",
-		fontWeight: 300,
-		fontSize: "3.5rem",
-		lineHeight: 1.2,
-		textAlign: "center",
-		marginBottom: "25px",
-	},
+  root: {
+    marginTop: "150px",
+    marginBottom: "200px",
+  },
+  title: {
+    fontFamily: "Teko",
+    color: "#f5f5f5",
+    textShadow: "1px 1px 3px rgba(0, 0, 0, 0.6)",
+    fontWeight: 300,
+    fontSize: "3.5rem",
+    lineHeight: 1.2,
+    textAlign: "center",
+    marginBottom: "25px",
+  },
 
-	subTitle: {
-		fontFamily: "Montserrat",
-		color: "#f5f5f5",
-		textShadow: "1px 1px 3px rgba(0, 0, 0, 0.6)",
-		fontWeight: 300,
-		fontSize: "1rem",
-		lineHeight: 1.2,
-		textAlign: "center",
-		marginBottom: "50px",
-	},
-	img: {
-		height: 0,
-		paddingTop: "56.25%", // 16:9,
-		marginTop: "30",
-		transition: "all 0.4s ease",
-	},
-	card: {
-		position: "relative",
-		maxWidth: 450,
-		minWidth: 250,
-		marginLeft: "20px",
-		transition: "all 0.4s ease",
-		cursor: "pointer",
-		marginBottom: "25px",
-		"&:hover": {
-			background:
-				" linear-gradient(56deg, rgba(255,46,99,1) 0%, rgba(232,0,59,1) 100%)",
-			opacity: "1",
-		},
-		"&:hover $img": {
-			opacity: 0.2,
-			zIndex: -1,
-		},
-		"&:hover $cardBack": {
-			opacity: 1,
-		},
-		[theme.breakpoints.down("xs")]: { maxWidth: "90%" },
-	},
+  subTitle: {
+    fontFamily: "Montserrat",
+    color: "#f5f5f5",
+    textShadow: "1px 1px 3px rgba(0, 0, 0, 0.6)",
+    fontWeight: 300,
+    fontSize: "1rem",
+    lineHeight: 1.2,
+    textAlign: "center",
+    marginBottom: "50px",
+  },
+  img: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9,
+    marginTop: "30",
+    transition: "all 0.4s ease",
+  },
+  card: {
+    position: "relative",
+    maxWidth: 450,
+    minWidth: 250,
+    marginLeft: "20px",
+    transition: "all 0.4s ease",
+    cursor: "pointer",
+    marginBottom: "25px",
+    "&:hover": {
+      background:
+        " linear-gradient(56deg, rgba(255,46,99,1) 0%, rgba(232,0,59,1) 100%)",
+      opacity: "1",
+    },
+    "&:hover $img": {
+      opacity: 0.2,
+      zIndex: -1,
+    },
+    "&:hover $cardBack": {
+      opacity: 1,
+    },
+    [theme.breakpoints.down("xs")]: { maxWidth: "90%" },
+  },
 
-	cardBack: {
-		top: "50%",
-		left: "50%",
-		transform: "translate(-50%, -50%)",
-		fontFamily: "teko",
-		textShadow: "1px 1px 5px rgba(0,0,0,0.4)",
-		opacity: 0,
-		position: "absolute",
-		transition: "all 0.4s ease",
-		fontSize: "2rem",
-		color: "#f5f5f5",
-		textAlign: "center",
+  cardBack: {
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    fontFamily: "teko",
+    textShadow: "1px 1px 5px rgba(0,0,0,0.4)",
+    opacity: 0,
+    position: "absolute",
+    transition: "all 0.4s ease",
+    fontSize: "1.3rem",
+    color: "#f5f5f5",
+    textAlign: "center",
 
-		[theme.breakpoints.down(1700)]: {
-			fontSize: "1.5rem",
-		},
-		[theme.breakpoints.down(1275)]: {
-			fontSize: "1.2rem",
-		},
-		[theme.breakpoints.down(650)]: {
-			fontSize: "2rem",
-		},
-		[theme.breakpoints.down(550)]: {
-			fontSize: "1.5rem",
-		},
-	},
+    [theme.breakpoints.down(1700)]: {
+      fontSize: "1.5rem",
+    },
+    [theme.breakpoints.down(1275)]: {
+      fontSize: "1.2rem",
+    },
+    [theme.breakpoints.down(650)]: {
+      fontSize: "2rem",
+    },
+    [theme.breakpoints.down(550)]: {
+      fontSize: "1.5rem",
+    },
+  },
 
-	cardRow: { maxWidth: "80%", margin: "0 50px", flexGrow: 1 },
+  cardRow: { maxWidth: "80%", margin: "0 50px", flexGrow: 1 },
 }));
 
 const MyWork = () => {
-	const classes = useStyles();
+  const classes = useStyles();
 
-	return (
-		<Fragment>
-			{console.log(projects)}
-			<Grid className={classes.root} container justify='center'>
-				<Grid item xs={12}>
-					<Typography className={classes.title}>My Work</Typography>
+  return (
+    <Fragment>
+      <Grid className={classes.root} container justify="center">
+        <Grid item xs={12}>
+          <Typography className={classes.title}>My Work</Typography>
 
-					<Typography className={classes.subTitle}>
-						Take a look at what I've been working on recently
-					</Typography>
-				</Grid>
+          <Typography className={classes.subTitle}>
+            Take a look at what I've been working on recently
+          </Typography>
+        </Grid>
 
-				<Grid
-					item
-					xs={12}
-					style={{ width: "100%" }}
-					container
-					direction='row'
-					justify='center'
-				>
-					<Grid container className={classes.cardRow} justify='center'>
-						{projects.map((item, i) => {
-							return (
-								<Grid item xs={12} sm={6} md={4}>
-									<Card className={classes.card}>
-										<CardActionArea href={item.link}>
-											<CardMedia
-												className={classes.img}
-												image={item.image}
-												alt={item.description}
-											></CardMedia>
-											<CardMedia className={classes.cardBack}>
-												{item.description}
-												<br /> Stack: <br />
-												{item.stack}
-											</CardMedia>
-										</CardActionArea>
-									</Card>
-								</Grid>
-							);
-						})}
-					</Grid>
-				</Grid>
-			</Grid>
-		</Fragment>
-	);
+        <Grid
+          item
+          xs={12}
+          style={{ width: "100%" }}
+          container
+          direction="row"
+          justify="center"
+        >
+          <Grid container className={classes.cardRow} justify="center">
+            {projects.map((item, i) => {
+              return (
+                <Grid item xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardActionArea href={item.link}>
+                      <CardMedia
+                        className={classes.img}
+                        image={item.image}
+                        alt={item.description}
+                      ></CardMedia>
+                      <CardMedia className={classes.cardBack}>
+                        {item.description}
+                        <br /> Stack: <br />
+                        {item.stack}
+                      </CardMedia>
+                    </CardActionArea>
+                  </Card>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Grid>
+      </Grid>
+    </Fragment>
+  );
 };
 
 export default MyWork;
--- src/components/SkillCards.js
+++ src/components/SkillCards.js
@@ -12,235 +12,240 @@ import { Spring } from "react-spring/renderprops";
 import VisibilitySensor from "react-visibility-sensor";
 
 const useStyles = makeStyles((theme) => ({
-	card: {
-		width: "300px",
-		padding: "20px",
-		margin: "0px 15px",
-		borderRadius: "15px",
-		boxShadow: "2px 2px 15px 2px rgba(0,0,0,0.3)",
-		marginBottom: "20px",
-	},
-	title: {
-		fontFamily: "Teko",
-		fontSize: "2rem",
-	},
+  card: {
+    width: "300px",
+    padding: "20px",
+    margin: "0px 15px",
+    borderRadius: "15px",
+    boxShadow: "2px 2px 15px 2px rgba(0,0,0,0.3)",
+    marginBottom: "20px",
+  },
+  title: {
+    fontFamily: "Teko",
+    fontSize: "2rem",
+  },
 
-	subTitle: {
-		fontFamily: "Montserrat",
-		fontSize: "1rem",
-		color: "#ff2e63",
-		marginTop: "30px",
-	},
+  subTitle: {
+    fontFamily: "Montserrat",
+    fontSize: "1rem",
+    color: "#ff2e63",
+    marginTop: "30px",
+  },
 
-	listItem: {
-		textAlign: "center",
-		fontSize: "0.8rem",
-		fontFamily: "Montserrat",
-		marginTop: "10px",
-	},
+  listItem: {
+    textAlign: "center",
+    fontSize: "0.8rem",
+    fontFamily: "Montserrat",
+    marginTop: "10px",
+  },
 
-	container: {
-		width: "100%",
-		justifyContent: "center",
-	},
+  container: {
+    width: "100%",
+    justifyContent: "center",
+  },
 
-	imgContainer: {
-		maxWidth: "80px",
-		margin: "0 auto",
-	},
+  imgContainer: {
+    maxWidth: "80px",
+    margin: "0 auto",
+  },
 
-	img: {
-		maxWidth: "100%",
-		maxHeight: "100%",
-	},
+  img: {
+    maxWidth: "100%",
+    maxHeight: "100%",
+  },
 }));
 
 const SkillCards = () => {
-	const classes = useStyles();
-	return (
-		<React.Fragment>
-			<Grid container justify='center'>
-				<Grid container justify='center' style={{ width: "80%" }}>
-					<VisibilitySensor partialVisibility={true} minTopValue={100}>
-						{({ isVisible }) => (
-							<Spring
-								to={{
-									opacity: isVisible ? 1 : 0,
-								}}
-							>
-								{(props) => (
-									<Grid
-										style={{ ...props }}
-										item
-										container
-										xs={12}
-										sm={6}
-										md={4}
-										justify='center'
-									>
-										<Card className={classes.card}>
-											<CardContent>
-												<Box className={classes.imgContainer}>
-													<img
-														className={classes.img}
-														src={iconBE}
-														alt='front-end development'
-													/>
-												</Box>
-												<Typography align='center' className={classes.title}>
-													Front-End Development
-												</Typography>
-												<Typography align='center' className={classes.subTitle}>
-													Languages I speak:
-												</Typography>
-												<Typography
-													align='center'
-													className={classes.listItem}
-													style={{ fontSize: "1rem" }}
-												>
-													HTML, CSS, JavaScript, ReactJS, NextJS, Angular
-												</Typography>
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Grid container justify="center">
+        <Grid container justify="center" style={{ width: "80%" }}>
+          <VisibilitySensor partialVisibility={true} minTopValue={100}>
+            {({ isVisible }) => (
+              <Spring
+                to={{
+                  opacity: isVisible ? 1 : 0,
+                }}
+              >
+                {(props) => (
+                  <Grid
+                    style={{ ...props }}
+                    item
+                    container
+                    xs={12}
+                    sm={6}
+                    md={4}
+                    justify="center"
+                  >
+                    <Card className={classes.card}>
+                      <CardContent>
+                        <Box className={classes.imgContainer}>
+                          <img
+                            className={classes.img}
+                            src={iconBE}
+                            alt="front-end development"
+                          />
+                        </Box>
+                        <Typography align="center" className={classes.title}>
+                          Front-End Development
+                        </Typography>
+                        <Typography align="center" className={classes.subTitle}>
+                          Languages I speak:
+                        </Typography>
+                        <Typography
+                          align="center"
+                          className={classes.listItem}
+                          style={{ fontSize: "1rem" }}
+                        >
+                          HTML, CSS, JavaScript, ReactJS, NextJS, Angular
+                        </Typography>
 
-												<Typography align='center' className={classes.subTitle}>
-													Tools i Use
-												</Typography>
-												<ul style={{ listStyleType: "none", padding: 0 }}>
-													<li className={classes.listItem}>
-														Material UI, Bootstrap,{" "}
-													</li>
+                        <Typography align="center" className={classes.subTitle}>
+                          Tools i Use
+                        </Typography>
+                        <ul style={{ listStyleType: "none", padding: 0 }}>
+                          <li className={classes.listItem}>
+                            Material UI, Bootstrap, AntD,
+                          </li>
 
-													<li className={classes.listItem}>
-														React Spring, Framer Motion
-													</li>
-													<li className={classes.listItem}>Adobe XD, Figma</li>
-													<li className={classes.listItem}>GitHUB</li>
-													<li className={classes.listItem}>Netlify, Vercel</li>
-												</ul>
-											</CardContent>
-										</Card>
-									</Grid>
-								)}
-							</Spring>
-						)}
-					</VisibilitySensor>
-					<VisibilitySensor partialVisibility={true} minTopValue={100}>
-						{({ isVisible }) => (
-							<Spring
-								to={{
-									opacity: isVisible ? 1 : 0,
-								}}
-								delay={150}
-							>
-								{(props) => (
-									<Grid
-										style={{ ...props }}
-										item
-										container
-										xs={12}
-										sm={6}
-										md={4}
-										justify='center'
-									>
-										<Card className={classes.card}>
-											<CardContent>
-												<Box className={classes.imgContainer}>
-													<img
-														className={classes.img}
-														src={iconFE}
-														alt='back-end development'
-													/>
-												</Box>
-												<Typography align='center' className={classes.title}>
-													Back-End Development
-												</Typography>
-												<Typography align='center' className={classes.subTitle}>
-													Languages I speak:
-												</Typography>
-												<Typography
-													align='center'
-													className={classes.listItem}
-													style={{ fontSize: "1rem" }}
-												>
-													NodeJs, NoSQL, SQL
-												</Typography>
+                          <li className={classes.listItem}>
+                            React Spring, Framer Motion, ReCharts
+                          </li>
+                          <li className={classes.listItem}>Adobe XD, Figma</li>
 
-												<Typography align='center' className={classes.subTitle}>
-													Tools i Use
-												</Typography>
-												<ul style={{ listStyleType: "none", padding: 0 }}>
-													<li className={classes.listItem}>Express</li>
-													<li className={classes.listItem}>MongoDB</li>
-													<li className={classes.listItem}>PostgreSQL</li>
-													<li className={classes.listItem}>GitHUB</li>
-													<li className={classes.listItem}>Heroku</li>
-												</ul>
-											</CardContent>
-										</Card>
-									</Grid>
-								)}
-							</Spring>
-						)}
-					</VisibilitySensor>
-					<VisibilitySensor partialVisibility={true} minTopValue={100}>
-						{({ isVisible }) => (
-							<Spring
-								to={{
-									opacity: isVisible ? 1 : 0,
-								}}
-								delay={250}
-							>
-								{(props) => (
-									<Grid
-										style={{ ...props }}
-										item
-										container
-										xs={12}
-										sm={6}
-										md={4}
-										justify='center'
-									>
-										<Card className={classes.card}>
-											<CardContent>
-												<Box className={classes.imgContainer}>
-													<img
-														className={classes.img}
-														src={iconPM}
-														alt='project management'
-													/>
-												</Box>
-												<Typography align='center' className={classes.title}>
-													Project-Mangement
-												</Typography>
-												<Typography align='center' className={classes.subTitle}>
-													Practices:
-												</Typography>
-												<Typography
-													align='center'
-													className={classes.listItem}
-													style={{ fontSize: "1rem" }}
-												>
-													Agile, SCRUM
-												</Typography>
+                          <li className={classes.listItem}>Netlify, Vercel</li>
+                        </ul>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                )}
+              </Spring>
+            )}
+          </VisibilitySensor>
+          <VisibilitySensor partialVisibility={true} minTopValue={100}>
+            {({ isVisible }) => (
+              <Spring
+                to={{
+                  opacity: isVisible ? 1 : 0,
+                }}
+                delay={150}
+              >
+                {(props) => (
+                  <Grid
+                    style={{ ...props }}
+                    item
+                    container
+                    xs={12}
+                    sm={6}
+                    md={4}
+                    justify="center"
+                  >
+                    <Card className={classes.card}>
+                      <CardContent>
+                        <Box className={classes.imgContainer}>
+                          <img
+                            className={classes.img}
+                            src={iconFE}
+                            alt="back-end development"
+                          />
+                        </Box>
+                        <Typography align="center" className={classes.title}>
+                          Back-End Development
+                        </Typography>
+                        <Typography align="center" className={classes.subTitle}>
+                          Languages I speak:
+                        </Typography>
+                        <Typography
+                          align="center"
+                          className={classes.listItem}
+                          style={{ fontSize: "1rem" }}
+                        >
+                          NodeJs, NoSQL, SQL
+                        </Typography>
 
-												<Typography align='center' className={classes.subTitle}>
-													Tools i Use
-												</Typography>
-												<ul style={{ listStyleType: "none", padding: 0 }}>
-													<li className={classes.listItem}>Trello</li>
-													<li className={classes.listItem}>Slack</li>
-													<li className={classes.listItem}>Discord</li>
-												</ul>
-											</CardContent>
-										</Card>
-									</Grid>
-								)}
-							</Spring>
-						)}
-					</VisibilitySensor>
-				</Grid>
-			</Grid>
-		</React.Fragment>
-	);
+                        <Typography align="center" className={classes.subTitle}>
+                          Tools i Use
+                        </Typography>
+                        <ul style={{ listStyleType: "none", padding: 0 }}>
+                          <li className={classes.listItem}>NodeJS, Express</li>
+                          <li className={classes.listItem}>MongoDB</li>
+                          <li className={classes.listItem}>
+                            PostgreSQL, MySQL
+                          </li>
+                          <li className={classes.listItem}>
+                            GitHub, BitBucket, Nginx
+                          </li>
+                          <li className={classes.listItem}>Heroku, Netfliy</li>
+                        </ul>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                )}
+              </Spring>
+            )}
+          </VisibilitySensor>
+          <VisibilitySensor partialVisibility={true} minTopValue={100}>
+            {({ isVisible }) => (
+              <Spring
+                to={{
+                  opacity: isVisible ? 1 : 0,
+                }}
+                delay={250}
+              >
+                {(props) => (
+                  <Grid
+                    style={{ ...props }}
+                    item
+                    container
+                    xs={12}
+                    sm={6}
+                    md={4}
+                    justify="center"
+                  >
+                    <Card className={classes.card}>
+                      <CardContent>
+                        <Box className={classes.imgContainer}>
+                          <img
+                            className={classes.img}
+                            src={iconPM}
+                            alt="project management"
+                          />
+                        </Box>
+                        <Typography align="center" className={classes.title}>
+                          Project-Mangement
+                        </Typography>
+                        <Typography align="center" className={classes.subTitle}>
+                          Practices:
+                        </Typography>
+                        <Typography
+                          align="center"
+                          className={classes.listItem}
+                          style={{ fontSize: "1rem" }}
+                        >
+                          Agile, SCRUM
+                        </Typography>
+
+                        <Typography align="center" className={classes.subTitle}>
+                          Tools i Use
+                        </Typography>
+                        <ul style={{ listStyleType: "none", padding: 0 }}>
+                          <li className={classes.listItem}>
+                            Trello, Jira, Monday
+                          </li>
+                          <li className={classes.listItem}>Slack, Discord</li>
+                        </ul>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                )}
+              </Spring>
+            )}
+          </VisibilitySensor>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
 };
 
 export default SkillCards;
